0b88b8586f2f544666d80c59817560060f4d6f78
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2022-06-21 20:45:02 +0530

    Time: 1100 ms (31.22%), Space: 14.9 MB (95.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        new_nums = [(target - num) for num in nums]
-        zip_list = list(zip(nums, new_nums))
-        for i, item in enumerate(zip_list):
-          a, b = item
-          if a in nums and b in nums[i+1:]:
-            return(nums.index(a), nums[i+1:].index(b)+i+1)
-        # for i, num in enumerate(nums):
-        #   if (target-num) in nums[i+1:]:
-        #     return(i, nums[i+1:].index(target-num)+i+1)
+        # combs = itertools.combinations(nums, 2)
+        # return[(nums.index(i[0]), nums[nums.index(i[0])+1:].index(i[1]) + nums.index(i[0]) +1) for i in combs if sum(i)==target][0]
+
+        for i, num in enumerate(nums):
+          if (target-num) in nums[i+1:]:
+            return(i, nums[i+1:].index(target-num)+i+1)
 
