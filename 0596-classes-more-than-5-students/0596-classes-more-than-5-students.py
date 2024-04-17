537a8383fab918745f55020566ca3138d470e709
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-04-17 22:50:16 +0530

    Time: 592 ms (60.88%), Space: 67.1 MB (27.86%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.py
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     grouped = courses.groupby("class").count().reset_index()
-    return grouped[grouped["student"] >= 5]["class"].to_frame()
+    return grouped[grouped["student"] >= 5][["class"]]
