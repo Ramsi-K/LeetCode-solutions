0a7dc5dc5bbf982de982121346bad0aade270c9b
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-04-24 21:47:30 +0530

    Time: 1118 ms (5%), Space: 67.9 MB (21.24%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather.sort_values("recordDate", inplace=True)
+    weather.set_index("recordDate", inplace=True)
+    if len(weather) >= 2:
+        weather = weather.reindex(pd.date_range(weather.index[0], weather.index[-1]))
+    weather["diff"] = weather["temperature"] - weather["temperature"].shift(1)
+    res = weather[weather["diff"] > 0]
+    return res[["id"]]
+    
