80c9febe82ce6345c50161c55b24f1323f11b6fa
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-04-24 20:22:58 +0530

    Time: 589 ms (65.68%), Space: 65.9 MB (54.14%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.py
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    res = person.groupby("email").agg(count = pd.NamedAgg("email", "count")).reset_index()
-    res = res[res["count"]>1]
-    return res[["email"]].rename(columns={"email":"Email"})
+    return person[person['email'].duplicated()][["email"]].drop_duplicates().rename(columns={"email":"Email"})
+ 
