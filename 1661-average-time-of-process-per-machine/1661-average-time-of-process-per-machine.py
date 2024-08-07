dbb9b6a3c6b2bdaf538c48af7d283a09460d3dba
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-08-07 05:38:24 +0530

    Time: 461 ms (20.34%), Space: 71.4 MB (8.14%) - LeetHub

---
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
+    out = activity.sort_values(["machine_id", "process_id", "timestamp"]).set_index(["machine_id", "process_id"]) 
+    print(out)
+    out = out.groupby(level=[0,1]).agg({"timestamp":"diff"}).round(3).dropna()
+    print(out)
+    out = out.groupby(level=0).mean().round(3).rename(columns={"timestamp": "processing_time"}).reset_index()
+    print(out)
+    return out
