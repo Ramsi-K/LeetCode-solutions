5a45c325f7c734f9eb84b6c1762f7b24daff16ce
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-08-13 08:51:20 +0530

    Time: 62 ms (92.77%), Space: 16.4 MB (87.69%) - LeetHub

---
+++ 3248-snake-in-matrix/3248-snake-in-matrix.py
@@ -0,0 +1,5 @@
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        my_dict = {"UP": -n, "DOWN": n, "RIGHT": 1, "LEFT": -1}
+        final_pos = sum([my_dict.get(item, item) for item in commands])
+        return final_pos
