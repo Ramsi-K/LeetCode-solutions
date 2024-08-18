eda12b36b84944f8319d72c045d71d5b56d50775
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-08-18 23:19:19 +0530

    Time: 39 ms (85.66%), Space: 17.1 MB (54.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,11 +2,10 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         translator = str.maketrans("", "", string.punctuation + string.whitespace)
         text = s.translate(translator).lower()
-        pointer_1 = 0
-        pointer_2 = len(text)-1
-        print(text)
+        pointer_1, pointer_2 = 0, len(text)-1
+        # print(text)
         while pointer_1 < pointer_2:
-            print(f"Pointer_1: {text[pointer_1]}, Pointer_2: {text[pointer_2]}")
+            # print(f"Pointer_1: {text[pointer_1]}, Pointer_2: {text[pointer_2]}")
             if text[pointer_1] == text[pointer_2]: 
                 pointer_1 += 1
                 pointer_2 -= 1
