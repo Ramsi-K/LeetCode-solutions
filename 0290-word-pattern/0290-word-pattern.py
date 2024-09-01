97c1f0b5b1267bbe7071b97a7bd2cdcb1861bcac
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-01 16:05:20 +0530

    Time: 34 ms (61.94%), Space: 16.5 MB (37.92%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        pattern = list(pattern)
+        s = s.split()
+
+        if len(set(pattern)) != len(set(s)): return False
+
+        translate_dict  = {pattern[i]: s[i] for i in range(len(pattern))}
+        trans = str.maketrans(translate_dict)
+        pattern_after = [s.translate(trans) for s in pattern]
+        # print(pattern)
+        # print(pattern_after)
+
+        return pattern_after == s
