aa121fc59f7c19f28f1f14c601d49f34177c4f90
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-02 14:52:10 +0530

    Time: 54 ms (43.96%), Space: 17.4 MB (71.34%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -6,21 +6,28 @@ class Solution:
         vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
         while pointer_1 < pointer_2:
-            if s[pointer_1] in vowels and s[pointer_2] in vowels:
+            if s[pointer_1] in vowels: 
+                if s[pointer_2] in vowels:
                 # print(f"S: {s} \nS-poin1: {s[pointer_1]} \nS-point2: {s[pointer_2]}")
                 # print(f"Swapping: {s[pointer_1], s[pointer_2]}")
-                s[pointer_1], s[pointer_2] = s[pointer_2], s[pointer_1]
-                pointer_1 += 1
-                pointer_2 -= 1
+                    s[pointer_1], s[pointer_2] = s[pointer_2], s[pointer_1]
+                    pointer_1 += 1
+                    pointer_2 -= 1
+                else:
+                    pointer_2 -= 1
+            # elif s[pointer_1] in vowels and s[pointer_2] not in vowels:
+            #     pointer_2 -= 1
 
-            elif s[pointer_1] in vowels and s[pointer_2] not in vowels:
-                pointer_2 -= 1
-
-            elif s[pointer_1] not in vowels and s[pointer_2] in vowels:
-                pointer_1 += 1
-
+            # elif s[pointer_1] not in vowels and s[pointer_2] in vowels:
             else:
-                pointer_1 += 1
-                pointer_2 -= 1
+                if s[pointer_2] in vowels:
+                    pointer_1 += 1
+                else:
+                    pointer_1 += 1
+                    pointer_2 -= 1
+
+            # else:
+            #     pointer_1 += 1
+            #     pointer_2 -= 1
         
         return "".join(s)
