64ef84e8d327c7ab6d0d294d9ea3477299530378
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-02 14:51:33 +0530

    Time: 70 ms (13.67%), Space: 17.6 MB (37.18%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -3,14 +3,12 @@ class Solution:
         s = list(s)
         pointer_1 = 0
         pointer_2 = len(s) - 1
-        vowels = ['a', 'e', 'i', 'o', 'u']
-
-        print(f"S: {s} \nS-poin1: {s[pointer_1]} \nS-point2: {s[pointer_2]}")
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
         while pointer_1 < pointer_2:
             if s[pointer_1] in vowels and s[pointer_2] in vowels:
-                print(f"S: {s} \nS-poin1: {s[pointer_1]} \nS-point2: {s[pointer_2]}")
-                print(f"Swapping: {s[pointer_1], s[pointer_2]}")
+                # print(f"S: {s} \nS-poin1: {s[pointer_1]} \nS-point2: {s[pointer_2]}")
+                # print(f"Swapping: {s[pointer_1], s[pointer_2]}")
                 s[pointer_1], s[pointer_2] = s[pointer_2], s[pointer_1]
                 pointer_1 += 1
                 pointer_2 -= 1
