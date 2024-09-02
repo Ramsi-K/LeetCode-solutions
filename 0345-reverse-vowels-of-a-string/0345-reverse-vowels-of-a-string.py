9658a81a75e32504b685f20fa2bcd567a6a1183a
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-02 14:49:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        s = list(s)
+        pointer_1 = 0
+        pointer_2 = len(s) - 1
+        vowels = ['a', 'e', 'i', 'o', 'u']
+
+        print(f"S: {s} \nS-poin1: {s[pointer_1]} \nS-point2: {s[pointer_2]}")
+
+        while pointer_1 < pointer_2:
+            if s[pointer_1] in vowels and s[pointer_2] in vowels:
+                print(f"S: {s} \nS-poin1: {s[pointer_1]} \nS-point2: {s[pointer_2]}")
+                print(f"Swapping: {s[pointer_1], s[pointer_2]}")
+                s[pointer_1], s[pointer_2] = s[pointer_2], s[pointer_1]
+                pointer_1 += 1
+                pointer_2 -= 1
+
+            elif s[pointer_1] in vowels and s[pointer_2] not in vowels:
+                pointer_2 -= 1
+
+            elif s[pointer_1] not in vowels and s[pointer_2] in vowels:
+                pointer_1 += 1
+
+            else:
+                pointer_1 += 1
+                pointer_2 -= 1
+        
+        return "".join(s)
