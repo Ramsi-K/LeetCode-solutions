8a27cc135eeea9103ed7e90875ec9d1a9b38402a
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-02 14:30:20 +0530

    Time: 180 ms (6.94%), Space: 20.8 MB (88.43%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,5 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
+        # s[:] = s[::-1]
+       
+        pointer_1 = 0
+        pointer_2 = len(s)-1
         
+        while pointer_1 < pointer_2:
+            s[pointer_1], s[pointer_2] = s[pointer_2], s[pointer_1]
+            pointer_1 += 1
+            pointer_2 -= 1
+        return s
