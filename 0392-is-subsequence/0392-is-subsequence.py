595452c53f8eb112d40e755e193eecb0f0f9ec62
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-06 23:11:49 +0530

    Time: 33 ms (75%), Space: 16.6 MB (65.48%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(t) < len(s): return False
+        out = []
+        for i in s:
+            # print(i)
+            ind = t.find(i)
+            out.append(ind)
+            t = t.replace(t[:ind+1], "%"*(ind+1), 1)
+            # print(t)
+        
+        if any(x == -1 for x in out): return False
+        # print(out)
+        # print(out == sorted(out))
+        return (out == sorted(out))
