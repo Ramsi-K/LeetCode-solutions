aec22c9d52a184b474a50631114d7fdc3fce351a
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-06 22:54:16 +0530

    Time: 64 ms (5.15%), Space: 16.5 MB (61.71%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,6 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        out = [x for x in t if t.count(x) > s.count(x)]
+        return [x for x in t if t.count(x) > s.count(x)][0]
         # print(out)
-        return out[0]
+        # return out[0]
         
