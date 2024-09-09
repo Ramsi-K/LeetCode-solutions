bfe3c9c5e9f980a28dba79d7d818783b54e9c77b
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-09 08:04:03 +0530

    Time: 147 ms (14.51%), Space: 17.5 MB (6.76%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,24 +1,17 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        # n = len(s)
-        # factors = [i for i in range(1, n) if n % i == 0]
+        n = len(s)
+        factors = [i for i in range(1, n) if n % i == 0]
 
-        # for i in factors[::-1]:
-        #     if i == 1:
-        #         subsets = list(s)
-        #         # print(subsets)
-        #     else:                    
-        #         chunks, chunk_size = n, n//i
-        #         subsets = [s[j:j+chunk_size] for j in range(0, chunks, chunk_size)]
-            
-        #     counts = {subsets[i]: subsets.count(subsets[i]) for i in range(len(subsets))}
-        #     # print(subsets)
-        #     # print(counts)
-        #     # print(len(counts))
+        for i in factors[::-1]:
+            if i == 1:
+                subsets = list(s)
+                print(subsets)
+                return subsets.count(subsets[0]) == n
 
-        #     if len(counts) == 1: return True
+            else:                    
+                chunks, chunk_size = n, n//i
+                subsets = [s[j:j+chunk_size] for j in range(0, chunks, chunk_size)]
+                if subsets.count(subsets[0]) == i : return True
         
-        # return False
-
-        check = (s + s)[1:-1].find(s)
-        return s[:check+1]
+        return False
