a907d098165bf57090d5c74bf2a6c7dd509e9e06
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-14 23:44:11 +0530

    Time: 22 ms (99.11%), Space: 16.5 MB (34.5%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.py
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.py
@@ -4,15 +4,10 @@ class Solution:
         if counter.get("A", 0) < 2:
             if counter.get("L", 0) < 3: return True
             else: 
-                # out = [True for i in range(len(s)-2) if s[i:i+3] == "LLL"]
-                # if out : return False
-                # else: return True
-                # print(out)
-                for i in range(len(s) - 2):
-                    if s[i:i+3] == "LLL":
-                        return False
-                return True
-        return False
+                out = [True for i in range(len(s)-2) if s[i:i+3] == "LLL"]
+                if out : return False
+                else: return True
+
 
 
         
