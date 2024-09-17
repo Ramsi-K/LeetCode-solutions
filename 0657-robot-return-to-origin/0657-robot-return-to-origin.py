05bf49aff2e4f81ae56dac8e34400451099ada20
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-17 07:58:45 +0530

    Time: 42 ms (84.27%), Space: 16.5 MB (52.6%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        d = {'U': 0, 'D': 0, 'R': 0, 'L': 0}
+        d.update(Counter(moves))
+        u_d, r_l = d['U'] - d['D'], d['R'] - d['L']
+        return u_d == r_l == 0
