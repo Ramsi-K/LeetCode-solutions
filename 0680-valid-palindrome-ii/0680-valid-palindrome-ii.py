fb28eec36b57ec37f04da6194c704782d7b493a8
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-17 15:09:30 +0530

    Time: 68 ms (94.24%), Space: 17 MB (20.17%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,16 +1,12 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        counts = Counter(s)
-        ones = Counter(counts.values())[1]
-        if ones > 2: return False
-        else:
-            p1 = 0
-            p2 = len(s)-1
-            while p1 <= p2:
-                if s[p1] != s[p2]:
-                    string1 = s[:p1] + s[p1+1:]
-                    string2 = s[:p2] + s[p2+1:]
-                    return string1 == string1[::-1] or string2 == string2[::-1]
-                p1 += 1
-                p2 -= 1
-            return True
+        p1 = 0
+        p2 = len(s)-1
+        while p1 <= p2:
+            if s[p1] != s[p2]:
+                string1 = s[:p1] + s[p1+1:]
+                string2 = s[:p2] + s[p2+1:]
+                return string1 == string1[::-1] or string2 == string2[::-1]
+            p1 += 1
+            p2 -= 1
+        return True
