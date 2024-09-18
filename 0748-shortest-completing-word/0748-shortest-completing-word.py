e9e6e2751d7be9ff0588b81da4b696c62b7dc30e
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-18 10:38:58 +0530

    Time: 109 ms (10.84%), Space: 17.1 MB (28.69%) - LeetHub

--- 0748-shortest-completing-word/0748-shortest-completing-word.py
+++ 0748-shortest-completing-word/0748-shortest-completing-word.py
@@ -4,21 +4,15 @@ class Solution:
         counts = Counter(lp_word)
         keys = set(counts.keys())
         out = []
-        print("Counts ", counts)
 
         for word in words:
             cnts = Counter(word)
-            print("WORD: ", word)
-            print("CNTS: ", cnts)
             keys_to_remove = set(cnts.keys()) - keys
-            print("KEYSTOREMOVE: ", keys_to_remove)
 
             for key in keys_to_remove:
                 del cnts[key]
-                print("CNTS: ", cnts)
             if cnts.keys() == counts.keys():
                 if all(cnts[k] >= counts[k] for k in cnts.keys()):                
                     out.append(word)
-        print(out)
 
         return min(out, key=len) if out else 0
