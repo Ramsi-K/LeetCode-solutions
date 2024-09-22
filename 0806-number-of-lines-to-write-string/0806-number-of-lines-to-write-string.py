3f7a47e0e2b85590d4ffafed0c3056fa2a245393
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-22 14:41:24 +0530

    Time: 42 ms (17.86%), Space: 16.5 MB (69.98%) - LeetHub

--- 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
         result = [1, 0]
-        letter_to_width = {letter: widths[i] for i, letter in enumerate(string.ascii_lowercase)}
 
         for char in s:
-            width = letter_to_width[char]
+            width = widths[ord(char) - ord('a')]
             if result[1] + width > 100:
                 result[0] += 1 
                 result[1] = width 
