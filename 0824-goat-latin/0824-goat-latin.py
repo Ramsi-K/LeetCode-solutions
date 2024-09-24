be9a62d12b309c00c270468b153e226605e9a1d7
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-24 08:18:48 +0530

    Time: 30 ms (85.14%), Space: 16.5 MB (73.51%) - LeetHub

--- 0824-goat-latin/0824-goat-latin.py
+++ 0824-goat-latin/0824-goat-latin.py
@@ -1,11 +1,13 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
+        vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
         words = sentence.split(" ")
 
         for i, word in enumerate(words):
-            if word[:1].lower() in ["a", "e", "i", "o", "u"]:
-                words[i] = word + "ma" + ("a" * (i+1))
+            suffix = "ma" + "a" * (i + 1)
+            if word[0] in vowels:
+                words[i] = word + suffix
             else:
-                words[i] = word[1:] + word[:1] + "ma" + ("a" * (i+1))
+                words[i] = word[1:] + word[0] + suffix
 
         return " ".join(words)
