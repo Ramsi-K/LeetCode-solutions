dcc7e75850d4fc05a8fcd8018a93f7910b366081
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-25 08:24:19 +0530

    Time: 35 ms (58.87%), Space: 16.7 MB (35.94%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,21 +1,33 @@
 class Solution:
+    # def backspaceCompare(self, s: str, t: str) -> bool:
+    #     stack_s = []
+    #     stack_t = []
+    #     for i, char in enumerate(s):
+    #         if char == "#":
+    #             if stack_s:
+    #                 stack_s.pop()
+    #         else:
+    #             stack_s.append(char)
+    #     # print(''.join(stack_s))
+
+    #     for i, char in enumerate(t):
+    #         if char == "#":
+    #             if stack_t:
+    #                 stack_t.pop()
+    #         else:
+    #             stack_t.append(char)
+    #     # print(''.join(stack_t))
+
+    #     return stack_s == stack_t
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stack_s = []
-        stack_t = []
-        for i, char in enumerate(s):
-            if char == "#":
-                if stack_s:
-                    stack_s.pop()
-            else:
-                stack_s.append(char)
-        # print(''.join(stack_s))
+        def process(string):
+            skip = 0
+            for char in reversed(string):
+                if char == "#":
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield char
 
-        for i, char in enumerate(t):
-            if char == "#":
-                if stack_t:
-                    stack_t.pop()
-            else:
-                stack_t.append(char)
-        # print(''.join(stack_t))
-
-        return stack_s == stack_t
+        return all(x == y for x, y in zip(process(s), process(t))) and len(list(process(s))) == len(list(process(t)))
