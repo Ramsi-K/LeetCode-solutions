87c0ab685b70c7fecae4cdc9a2894159611b28e6
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-09-29 05:25:02 +0530

    Time: 53 ms (67.75%), Space: 16.7 MB (14.94%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -3,9 +3,20 @@ class Solution:
         unique_emails = set()
         
         for email in emails:
-            local, domain = email.split("@")
-            local = local.split("+")[0].replace(".", "")
-            unique_emails.add(f"{local}@{domain}")
+            # Find the index of '@' and '+' characters
+            at_index = email.find('@')
+            plus_index = email.find('+')
+
+            # Extract the local and domain parts
+            local = email[:plus_index] if plus_index != -1 else email[:at_index]
+            domain = email[at_index:]
+
+            # Remove all '.' from the local part
+            local = local.replace('.', '')
+
+            # Add the processed email (local@domain) to the set
+            unique_emails.add(local + domain)
+
 
         # print(len(unique_emails))
         return len(unique_emails)
