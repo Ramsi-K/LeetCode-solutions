81c3e264cf684fdbbd3005d1ff40638e46ad44a5
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-10-12 15:46:05 +0530

    Time: 72 ms (47.07%), Space: 17.9 MB (61.99%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,36 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
         result = 0
-        sign = 1
-        num_stack = []
-        paren_stack = []
+        sign = 1 
+        paren_stack = []  
+        num = 0  
 
-        for num in s:
-            if num.isdigit():
-                num_stack.append(num)
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
 
-            elif num == "+" or num == "-":
-                if num_stack:
-                    result += int(''.join(num_stack)) * sign
-                    num_stack.clear()  
-                sign = 1 if num == "+" else -1
+            elif char == "+":
+                result += sign * num
+                num = 0  
+                sign = 1  
 
-            elif num == "(":
+            elif char == "-":
+                result += sign * num
+                num = 0  
+                sign = -1  
+
+            elif char == "(":
                 paren_stack.append((result, sign))
                 result, sign = 0, 1
 
-            elif num == ")":
-                if num_stack:
-                    result += int(''.join(num_stack)) * sign
-                    num_stack.clear()
-                    
+            elif char == ")":
+                result += sign * num
+                num = 0  
+                
                 last_result, last_sign = paren_stack.pop()
                 result = last_result + last_sign * result
 
-            elif num == " ":
-                continue
+        result += sign * num
 
-        if num_stack:
-            result += int(''.join(num_stack)) * sign
-        # print(result)
         return result
