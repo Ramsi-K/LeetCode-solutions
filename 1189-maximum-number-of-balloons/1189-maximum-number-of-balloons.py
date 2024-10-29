06a006dc7ab9c126e8916793ab952eb84d3a936b
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2024-10-29 07:23:58 +0530

    Time: 0 ms (100%), Space: 16.7 MB (13.01%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -2,10 +2,7 @@ class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
         requirements = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
 
-        # Count occurrences of each required character in `text`
-        for char in text:
-            if char in requirements:
-                requirements[char] += 1
+        requirements.update(Counter(text))
 
         # Calculate the maximum number of "balloon" words we can form
         # Dividing by the required number for 'l' and 'o' (both needed twice)
