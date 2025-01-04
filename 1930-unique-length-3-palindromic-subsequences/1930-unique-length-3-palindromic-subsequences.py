a9b59ebf6599eaca01762f3b193c5cdfc99ec9fb
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-04 10:17:50 +0530

    Time: 115 ms (86.81%), Space: 18 MB (39.34%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,20 +1,15 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         unique_palindromes = set()
-            
+        
         for char in set(s):
+            # Find all positions of the character in the string
             first = s.find(char)
             last = s.rfind(char)
             
+            # If there's space for a middle character
             if last - first > 1:
-                # Create a bitmask for intermediate characters
-                bitmask = 0
-                for mid_char in s[first + 1 : last]:
-                    bitmask |= (1 << (ord(mid_char) - ord('a')))
-                
-                # Check all possible middle characters
-                for i in range(26):
-                    if bitmask & (1 << i):
-                        unique_palindromes.add(char + chr(i + ord('a')) + char)
+                for mid_char in set(s[first + 1 : last]):
+                    unique_palindromes.add(char + mid_char + char)
         
         return len(unique_palindromes)
