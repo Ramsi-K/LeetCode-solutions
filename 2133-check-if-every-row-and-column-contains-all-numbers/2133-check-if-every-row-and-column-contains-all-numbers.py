5532ed22ab1ec23d2d0456f4f422fae91ede786d
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-20 07:35:54 +0530

    Time: 27 ms (61.62%), Space: 18.8 MB (17.97%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -6,7 +6,9 @@ class Solution:
         # Check rows and columns
         # Use zip to access columns
         for row, col in zip(matrix, zip(*matrix)):
+            if len(set(row)) != n or len(set(col)) != n:
+                return False  # Early break if length doesn't match
             if set(row) != required_set or set(col) != required_set:
-                return False
+                return False  # Check if the values match the required set
 
         return True
