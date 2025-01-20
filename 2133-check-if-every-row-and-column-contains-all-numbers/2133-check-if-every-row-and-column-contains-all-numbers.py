2193bb899fd4087179a4761860032dd51f3837b7
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-20 07:33:41 +0530

    Time: 15 ms (88.73%), Space: 18.7 MB (28.53%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -4,9 +4,9 @@ class Solution:
         required_set = set(range(1, n + 1))  # The set {1, 2, ..., n}
 
         # Check rows and columns
-        for i in range(n):
-            # Validate row and column using set comprehension
-            if set(matrix[i]) != required_set or set(matrix[j][i] for j in range(n)) != required_set:
+        # Use zip to access columns
+        for row, col in zip(matrix, zip(*matrix)):
+            if set(row) != required_set or set(col) != required_set:
                 return False
 
         return True
