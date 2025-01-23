c01b90eb1ad19cf210546ece44f2d9c8f7312f91
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-23 18:03:16 +0530

    Time: 199 ms (5.24%), Space: 20.1 MB (11.61%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -2,22 +2,35 @@ class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
+        visited = set()
         
-        row_count = [0] * rows
-        col_count = [0] * cols
+        def bfs(r, c):
+            queue = deque([(r, c)])
+            visited.add((r, c))
+            servers = [(r, c)]
+            
+            while queue:
+                x, y = queue.popleft()
+                for i in range(rows):
+                    if grid[i][y] == 1 and (i, y) not in visited:
+                        visited.add((i, y))
+                        queue.append((i, y))
+                        servers.append((i, y))
+                for j in range(cols):
+                    if grid[x][j] == 1 and (x, j) not in visited:
+                        visited.add((x, j))
+                        queue.append((x, j))
+                        servers.append((x, j))
+            
+            if len(servers) > 1:
+                return len(servers)
+            return 0
         
-        # Count servers in each row and column
+        total_servers = 0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1:
-                    row_count[i] += 1
-                    col_count[j] += 1
+                if grid[i][j] == 1 and (i, j) not in visited:
+                    total_servers += bfs(i, j)
         
-        # Count servers that can communicate
-        communicating_servers = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1 and (row_count[i] > 1 or col_count[j] > 1):
-                    communicating_servers += 1
-        
-        return communicating_servers
+        return total_servers
+
