882b781daa615937927fcbdf24696c89700fa00f
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-23 18:03:23 +0530

    Time: 3 ms (98.88%), Space: 19.4 MB (74.16%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -1,36 +1,27 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        
-        def bfs(r, c):
-            queue = deque([(r, c)])
-            visited.add((r, c))
-            servers = [(r, c)]
-            
-            while queue:
-                x, y = queue.popleft()
-                for i in range(rows):
-                    if grid[i][y] == 1 and (i, y) not in visited:
-                        visited.add((i, y))
-                        queue.append((i, y))
-                        servers.append((i, y))
-                for j in range(cols):
-                    if grid[x][j] == 1 and (x, j) not in visited:
-                        visited.add((x, j))
-                        queue.append((x, j))
-                        servers.append((x, j))
-            
-            if len(servers) > 1:
-                return len(servers)
-            return 0
-        
-        total_servers = 0
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 1 and (i, j) not in visited:
-                    total_servers += bfs(i, j)
-        
-        return total_servers
+        num_rows = len(grid) 
+        total_communicating_servers = 0 
 
+        for row_index in range(num_rows):
+            row_sum = sum(grid[row_index]) 
+
+            # If more than one server is present in the row, they can communicate
+            if row_sum > 1:
+                total_communicating_servers += row_sum
+                continue
+
+            # If there is exactly one server in the row, check the column
+            elif row_sum == 1:
+                column_sum = 0  
+                column_index = grid[row_index].index(1) 
+
+                # Sum the servers in the current column
+                for row in range(num_rows):
+                    column_sum += grid[row][column_index]
+
+                    if column_sum > 1:
+                        total_communicating_servers += 1
+                        break
+
+        return total_communicating_servers
