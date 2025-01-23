16179d7692e19d05620795ee323f92abc9aab50f
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-23 18:00:54 +0530

    Time: 19 ms (52.43%), Space: 19.5 MB (60.67%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -21,16 +21,3 @@ class Solution:
                     communicating_servers += 1
         
         return communicating_servers
-        # rows = len(grid)
-        # cols = len(grid[0])
-        
-        # row_sum = [sum(grid[i]) for i in range(rows)]
-        # col_sum = [sum(grid[i][j] for i in range(rows)) for j in range(cols)]
-        
-        # communicating_servers = 0
-        # for i in range(rows):
-        #     for j in range(cols):
-        #         if grid[i][j] == 1 and (row_sum[i] > 1 or col_sum[j] > 1):
-        #             communicating_servers += 1
-        
-        # return communicating_servers
