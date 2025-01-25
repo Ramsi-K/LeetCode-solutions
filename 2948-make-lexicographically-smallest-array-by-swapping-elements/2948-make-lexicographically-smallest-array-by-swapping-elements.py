33ae3f00ae5668a5f309ca86ff4fba0f8cf60702
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-01-25 10:22:31 +0530

    Time: 288 ms (81.25%), Space: 44.6 MB (92.36%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,50 +1,29 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        from collections import defaultdict
-        
-        class UnionFind:
-            def __init__(self, size):
-                self.parent = list(range(size))
-                self.rank = [1] * size
-
-            def find(self, x):
-                if self.parent[x] != x:
-                    self.parent[x] = self.find(self.parent[x])
-                return self.parent[x]
-
-            def union(self, x, y):
-                root_x = self.find(x)
-                root_y = self.find(y)
-                if root_x != root_y:
-                    if self.rank[root_x] > self.rank[root_y]:
-                        self.parent[root_y] = root_x
-                    elif self.rank[root_x] < self.rank[root_y]:
-                        self.parent[root_x] = root_y
-                    else:
-                        self.parent[root_y] = root_x
-                        self.rank[root_x] += 1
-
         n = len(nums)
-        uf = UnionFind(n)
+        # Pair each number with its index and sort by the number
+        sorted_enum = sorted((num, i) for i, num in enumerate(nums))
         
-        # Sort the array based on the values
-        sorted_indices = sorted(range(n), key=lambda x: nums[x])
+        new_positions = []
+        curr_positions = []
+        prev = float('-inf')
         
-        # Union adjacent indices in the sorted array if their difference <= limit
-        for i in range(n - 1):
-            if abs(nums[sorted_indices[i]] - nums[sorted_indices[i + 1]]) <= limit:
-                uf.union(sorted_indices[i], sorted_indices[i + 1])
+        for num, idx in sorted_enum:
+            # If the current number exceeds the previous number by more than the limit,
+            # sort and append the current positions to the result
+            if num > prev + limit:
+                new_positions.extend(sorted(curr_positions))
+                curr_positions = [idx]
+            else:
+                curr_positions.append(idx)
+            prev = num
         
-        # Group indices by their connected components
-        groups = defaultdict(list)
-        for i in range(n):
-            groups[uf.find(i)].append(i)
+        # Append any remaining positions
+        new_positions.extend(sorted(curr_positions))
         
-        # Sort values within each group and place them back in the array
-        result = nums[:]
-        for group in groups.values():
-            values = sorted(result[i] for i in group)
-            for idx, val in zip(sorted(group), values):
-                result[idx] = val
-
-        return result
+        # Construct the result array using the new positions
+        res = [0] * n
+        for i, idx in enumerate(new_positions):
+            res[idx] = sorted_enum[i][0]
+        
+        return res
