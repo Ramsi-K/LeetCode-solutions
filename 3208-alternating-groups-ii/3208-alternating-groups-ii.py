609562bd0e70d1ea46ee3424f9909deb78b422c8
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-03-09 23:50:31 +0530

    Time: 666 ms (98.01%), Space: 21.2 MB (72.76%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.py
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.py
@@ -1,17 +1,19 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        l = []
         n = len(colors)
-        count = 0
-        left = 0
+        for i in range(n - 1):
+            if colors[i] == colors[i + 1]:
+                l.append(i)
 
-        # Iterate through the extended array (n + k - 1)
-        for right in range(n + k - 1):
-            # If alternation breaks, move the left pointer
-            if right > 0 and colors[right % n] == colors[(right - 1) % n]:
-                left = right
-            
-            # If the window size is at least k, increment the count
-            if right - left + 1 >= k:
-                count += 1
-        
-        return count
+        if colors[n - 1] == colors[0]:
+            l.append(n - 1)
+        if len(l) == 0:
+            return n
+
+        l.append(l[0] + n)
+        res = 0
+        for i in range(len(l) - 1):
+            res += max(0, l[i + 1] - l[i] - k + 1)
+
+        return res
