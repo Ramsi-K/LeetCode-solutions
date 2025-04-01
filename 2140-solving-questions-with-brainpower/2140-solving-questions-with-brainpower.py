7247d03f2b5ef92353616c3863dab7aac460d31b
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-04-01 15:37:53 +0530

    Time: 57 ms (98.38%), Space: 60.9 MB (66.88%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            points, skip = questions[i]
+            next_q = i + skip + 1
+            solve = points + (dp[next_q] if next_q < n else 0)
+            dp[i] = max(solve, dp[i + 1])
+
+        return dp[0]
