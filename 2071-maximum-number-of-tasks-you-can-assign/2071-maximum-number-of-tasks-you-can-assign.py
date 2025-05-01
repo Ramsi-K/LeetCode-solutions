ed4490921c1cabee6b8d42788dd12ab8627a3021
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-05-01 21:06:00 +0530

    Time: 267 ms (100%), Space: 26.2 MB (37.93%) - LeetHub

--- 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
@@ -1,69 +1,35 @@
+from collections import deque
+
 class Solution:
-    def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
+    def maxTaskAssign(self, tasks, workers, pills, strength):
         tasks.sort()
         workers.sort()
 
-        n = len(tasks)
-        m = len(workers)
-        
-        left, right = 0, min(n, m)
+        def can_assign(mid):
+            boosted = deque()
+            w = len(workers) - 1
+            free_pills = pills
 
-        def can_complete(k):
-            # Take the k smallest tasks
-            curr_tasks = tasks[:k]
-            
-            # Use a list to track which workers are available
-            available_workers = workers.copy()
-            remaining_pills = pills
-            
-            # Try to assign tasks from hardest to easiest
-            for i in range(k-1, -1, -1):
-                task = curr_tasks[i]
-                assigned = False
-                
-                # First try to assign without using a pill
-                # Find the weakest worker who can complete the task
-                worker_idx = -1
-                for j in range(len(available_workers)):
-                    if available_workers[j] >= task:
-                        worker_idx = j
-                        break
-                
-                if worker_idx != -1:
-                    # Found a worker who can do the task without a pill
-                    assigned = True
-                    # Remove this worker from available workers
-                    available_workers.pop(worker_idx)
-                    continue
-                
-                # If we can't assign without a pill, try with a pill
-                if remaining_pills > 0:
-                    # Find the weakest worker who can complete the task with a pill
-                    worker_idx = -1
-                    for j in range(len(available_workers)):
-                        if available_workers[j] + strength >= task:
-                            worker_idx = j
-                            break
-                    
-                    if worker_idx != -1:
-                        # Found a worker who can do the task with a pill
-                        assigned = True
-                        # Remove this worker from available workers
-                        available_workers.pop(worker_idx)
-                        remaining_pills -= 1
-                
-                if not assigned:
-                    # Cannot assign this task
-                    return False
-            
+            for t in reversed(tasks[:mid]):
+                if boosted and boosted[0] >= t:
+                    boosted.popleft()
+                elif w >= 0 and workers[w] >= t:
+                    w -= 1
+                else:
+                    while w >= 0 and workers[w] + strength >= t:
+                        boosted.append(workers[w])
+                        w -= 1
+                    if not boosted or free_pills == 0:
+                        return False
+                    boosted.pop()
+                    free_pills -= 1
             return True
-        
-        # Binary search for the maximum number of tasks
-        while left <= right:
-            mid = (left + right) // 2
-            if can_complete(mid):
-                left = mid + 1
+
+        low, high = 0, min(len(tasks), len(workers))
+        while low < high:
+            mid = (low + high + 1) // 2
+            if can_assign(mid):
+                low = mid
             else:
-                right = mid - 1
-        
-        return right
+                high = mid - 1
+        return low
