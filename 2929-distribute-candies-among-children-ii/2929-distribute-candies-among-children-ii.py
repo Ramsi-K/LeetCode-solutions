0bd454fc9c950f08fdeb9748fae6a3e7d9fe90b5
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-06-01 19:28:12 +0530

    Time: 5613 ms (5.48%), Space: 17.8 MB (48.63%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.py
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
         count = 0
-        for a in range(0, limit + 1):
-            for b in range(0, limit + 1):
-                c = n - a - b
-                if 0 <= c <= limit:
-                    count += 1
+        for a in range(max(0, n - 2 * limit), min(limit, n) + 1):
+            remaining = n - a
+            min_b = max(0, remaining - limit)
+            max_b = min(limit, remaining)
+            count += max(0, (max_b - min_b + 1))
         return count
