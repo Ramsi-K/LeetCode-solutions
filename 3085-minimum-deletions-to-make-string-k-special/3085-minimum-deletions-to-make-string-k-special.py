c2c66d8bd388aabc402ba005431763b8055bd99a
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-06-21 23:39:46 +0530

    Time: 59 ms (62.79%), Space: 18.1 MB (45.74%) - LeetHub

--- 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
@@ -1,25 +1,20 @@
+import collections
+
 class Solution:
     def minimumDeletions(self, word: str, k: int) -> int:
-        freqMap = defaultdict(int)
-        for c in word:
-            freqMap[c] += 1
-        
-        frequencies = sorted(freqMap.values())
-        minDeletions = float('inf')
-        n = len(frequencies)
-        
-        for i in range(n):
-            base = frequencies[i]
-            totalDeletions = 0
-            
-            for j in range(i):
-                totalDeletions += frequencies[j]
-            
-            for j in range(i, n):
-                if frequencies[j] > base + k:
-                    totalDeletions += frequencies[j] - (base + k)
-            
-            if totalDeletions < minDeletions:
-                minDeletions = totalDeletions
-        
-        return minDeletions
+        counts = sorted(collections.Counter(word).values())
+
+        def get_min_deletions(i):
+            if i == len(counts):
+                return float('inf')
+            smallest = counts[i]
+
+            result = 0
+            for j in reversed(range(i + 1, len(counts))):
+                delta = counts[j] - (smallest + k)
+                if delta <= 0:
+                    break
+                result += delta
+            return min(result, smallest + get_min_deletions(i + 1))
+
+        return get_min_deletions(0)
