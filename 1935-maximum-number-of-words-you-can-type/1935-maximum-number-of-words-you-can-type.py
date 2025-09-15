889a15b5dd9fef84b3bd5bb983dbffda66f54c96
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-09-15 20:15:28 +0530

    Time: 0 ms (100%), Space: 17.9 MB (57.65%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.py
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.py
@@ -1,10 +1,9 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
-        words = text.split(" ")
-        letters = list(brokenLetters)
-        count = len(words)
+        broken = set(brokenLetters) 
+        words = text.split()
+        count = 0
         for word in words:
-            if any(letter in letters for letter in word):
-                count -= 1
-        
+            if not (set(word) & broken):  
+                count += 1
         return count
