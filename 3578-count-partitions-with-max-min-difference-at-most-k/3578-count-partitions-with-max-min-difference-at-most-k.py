3adab0944f28d121078cd7d819b267c990b0ef4b
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-12-07 03:53:43 +0530

    Time: 393 ms (78.12%), Space: 24.9 MB (75%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.py
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.py
@@ -1,24 +1,8 @@
 class Solution:
     def countPartitions(self, nums: List[int], k: int) -> int:
-        mod = pow(10, 9) + 7
+        mod = pow(10,9)+7
         n = len(nums)
-        dp = [0] * n
-        dp[0] = 1
-        for i in range(1, n):
-            largest = smallest = nums[i]
-            for j in range(i, -1, -1):
-                largest = max(largest, nums[j])
-                smallest = min(smallest, nums[j])
-                if largest - smallest > k:
-                    break
-                dp[i] += dp[j - 1] if j > 0 else 1
-                dp[i] %= mod
-        return dp[-1]
-
-    def countPartitions(self, nums: List[int], k: int) -> int:
-        mod = pow(10, 9) + 7
-        n = len(nums)
-        dp = [1] + [0] * n
+        dp = [1]+[0]*n
         largest = deque()
         smallest = deque()
         acc = 1
@@ -31,13 +15,13 @@ class Solution:
                 smallest.pop()
             smallest.append(i)
 
-            while nums[largest[0]] - nums[smallest[0]] > k:
-                acc = (acc - dp[j]) % mod
+            while nums[largest[0]]-nums[smallest[0]] > k:
+                acc = (acc-dp[j]) % mod
                 j += 1
                 if largest[0] < j:
                     largest.popleft()
                 if smallest[0] < j:
                     smallest.popleft()
-            dp[i + 1] = acc
-            acc = (acc + dp[i + 1]) % mod
+            dp[i+1] = acc
+            acc = (acc+dp[i+1]) % mod
         return dp[-1]
