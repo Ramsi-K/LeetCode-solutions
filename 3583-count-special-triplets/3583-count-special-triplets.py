57caab62d3001c3accdc096d59908256b7f25a77
Ramsi Kalia <85371945+Ramsi-K@users.noreply.github.com>
2025-12-10 02:17:16 +0530

    Time: 273 ms (100%), Space: 35.8 MB (96.46%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.py
+++ 3583-count-special-triplets/3583-count-special-triplets.py
@@ -1,14 +1,25 @@
+from typing import List
+
 class Solution:
     def specialTriplets(self, nums: List[int]) -> int:
-        memo, duplets = {}, {}
-        res = 0
+        # Count of single numbers seen so far
+        singles = {}
+        # Count of valid pair sums encountered so far
+        pairs = {}
+
         MOD = 10**9 + 7
+        result = 0
 
-        for number in nums:
-            res = (res + duplets.get(number, 0)) % MOD
-            twon = number * 2
-            if twon in memo:
-                duplets[twon] = (duplets.get(twon, 0) + memo[twon]) % MOD
-            memo[number] = memo.get(number, 0) + 1
+        for x in nums:
+            # Any pair matching x completes a triplet
+            result = (result + pairs.get(x, 0)) % MOD
 
-        return res
+            doubled = x * 2
+
+            # every occurrence forms a new valid pair (doubled, x)
+            if doubled in singles:
+                pairs[doubled] = (pairs.get(doubled, 0) + singles[doubled]) % MOD
+
+            singles[x] = singles.get(x, 0) + 1
+
+        return result
